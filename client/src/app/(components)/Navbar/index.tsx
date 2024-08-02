f370ad3c7a7848efb579d8693dadc781f40681a3
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-02 09:37:43 +0530

    feat: Add Sidebar component

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -7,6 +7,7 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center w-full mb-7'>
+      {/* LEFT AREA */}
       <div className='flex justify-between items-center gap-5'>
         <button
           className='px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100'
@@ -27,7 +28,7 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-
+      {/* RIGHT AREA */}
       <div className='flex justify-between items-center gap-5'>
         <div className='hidden md:flex justify-between items-center gap-5'>
           <div>
