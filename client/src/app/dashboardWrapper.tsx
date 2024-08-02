cfeb00de3f22fb3668f45c50b08eb428cd1b45c5
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-02 09:26:26 +0530

    feat: Add Navbar component and Dashboard page

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/app/(components)/Navbar';
+import React from 'react';
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='light flex bg-gray-50 text-gray-900 w-full min-h-screen'>
+      <main className='flex flex-col w-full h-full py-7 px-9 bg-gray-50 lg:pl-72 md:pl-24'>
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardWrapper;
