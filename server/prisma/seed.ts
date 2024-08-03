190cfeb09e5b6b0460f0c6c148ccdd7b52b02194
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-03 21:27:21 +0530

    feat: add pre-commit hook and eslint

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,60 +1,70 @@
-import {PrismaClient} from "@prisma/client";
-import path from "path"
-import * as fs from "node:fs";
+import { PrismaClient } from '@prisma/client';
+import path from 'path';
+import * as fs from 'node:fs';
 
 const prisma = new PrismaClient();
 
 async function deleteAllData(orderedFileNames: string[]) {
-  const modelNames = orderedFileNames.map((fileName) => {
-    const modelName = path.basename(fileName, path.extname(fileName));
-    return modelName.charAt(0).toUpperCase() + modelName.slice(1);
-  });
+	const modelNames = orderedFileNames.map((fileName) => {
+		const modelName = path.basename(fileName, path.extname(fileName));
+		return modelName.charAt(0).toUpperCase() + modelName.slice(1);
+	});
 
-  for (const modelName of modelNames) {
-    const model: any = prisma[modelName as keyof typeof prisma];
-    if (model) {
-      await model.deleteMany({});
-      console.log(`Cleared data from ${modelName}`);
-    } else {
-      console.error(`Model ${modelName} not found. Please ensure the model name is correctly specified`);
-    }
-  }
+	await Promise.all(
+		modelNames.map(async (modelName) => {
+			const model: any = prisma[modelName as keyof typeof prisma];
+			if (model) {
+				await model.deleteMany({});
+				console.log(`Cleared data from ${modelName}`);
+			} else {
+				console.error(
+					`Model ${modelName} not found. Please ensure the model name is correctly specified`,
+				);
+			}
+		}),
+	);
 }
 
 async function main() {
-  const dataDirectory = path.join(__dirname, "seedData");
+	const dataDirectory = path.join(__dirname, 'seedData');
 
-  const orderedFileNames = [
-    "products.json",
-    "expenseSummary.json",
-    "sales.json",
-    "salesSummary.json",
-    "purchases.json",
-    "purchaseSummary.json",
-    "users.json",
-    "expenses.json",
-    "expenseByCategory.json",
-  ];
+	const orderedFileNames = [
+		'products.json',
+		'expenseSummary.json',
+		'sales.json',
+		'salesSummary.json',
+		'purchases.json',
+		'purchaseSummary.json',
+		'users.json',
+		'expenses.json',
+		'expenseByCategory.json',
+	];
 
-  await deleteAllData(orderedFileNames);
+	await deleteAllData(orderedFileNames);
 
-  for (const fileName of orderedFileNames) {
-    const filePath = path.join(dataDirectory, fileName);
-    const jsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-    const modelName = path.basename(fileName, path.extname(fileName));
-    const model: any = prisma[modelName as keyof typeof prisma];
+	await Promise.all(
+		orderedFileNames.map(async (fileName) => {
+			const filePath = path.join(dataDirectory, fileName);
+			const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+			const modelName = path.basename(fileName, path.extname(fileName));
+			const model: any = prisma[modelName as keyof typeof prisma];
 
-    if (!model) {
-      console.error(`No Prisma model matches the file name: ${fileName}`);
-      continue;
-    }
+			if (!model) {
+				console.error(`No Prisma model matches the file name: ${fileName}`);
+				return;
+			}
 
-    for (const data of jsonData) {
-      await model.create({data});
-    }
+			await Promise.all(
+				jsonData.map(async (data: any) => {
+					await model.create({ data });
+				}),
+			);
 
-    console.log(`Seeded ${modelName} with data from ${fileName}`)
-  }
+			console.log(`Seeded ${modelName} with data from ${fileName}`);
+		}),
+	);
 }
 
-main().catch((error) => console.error(error)).finally(async () => await prisma.$disconnect());
+main()
+	.catch((error) => console.error(error))
+	.finally(async () => prisma.$disconnect());
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import helmet from "helmet";
-import morgan from "morgan";
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 // Load environment variables from .env file, where API keys and passwords are configured
 dotenv.config();
@@ -12,9 +12,9 @@ const app = express();
 
 // Express configuration
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(cors());
 
