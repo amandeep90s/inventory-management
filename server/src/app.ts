b12bd9677793f7710712fa42dc9f4b66d1430e91
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-03 21:05:19 +0530

    chore: added the express server app

---
+++ server/src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from "helmet";
+import morgan from "morgan";
+
+// Load environment variables from .env file, where API keys and passwords are configured
+dotenv.config();
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
+app.use(morgan('common'));
+app.use(cors());
+
+export default app;
---
+++ server/src/index.ts
@@ -0,0 +1,6 @@
+import app from "./app";
+
+// Start the server
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running at http://localhost:${process.env.SERVER_PORT}`);
+})
