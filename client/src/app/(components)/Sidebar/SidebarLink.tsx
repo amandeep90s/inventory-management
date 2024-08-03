efb72d4b4bbfc038388e6d188afad46f30e20c4d
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-03 21:47:00 +0530

    chore: added the prettier configuration for client

--- client/src/app/(components)/Sidebar/SidebarLink.tsx
+++ client/src/app/(components)/Sidebar/SidebarLink.tsx
@@ -1,29 +1,32 @@
 'use client';
 
-import {LucideIcon} from "lucide-react";
-import {usePathname} from "next/navigation";
-import Link from "next/link";
+import { LucideIcon } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 interface SidebarLinkProps {
-  href: string;
-  icon: LucideIcon;
-  label: string;
-  isCollapsed: boolean;
+	href: string;
+	icon: LucideIcon;
+	label: string;
+	isCollapsed: boolean;
 }
 
-const SidebarLink = ({href, icon: Icon, label, isCollapsed}: SidebarLinkProps) => {
-  const pathname = usePathname();
-  const isActive = pathname === href || (pathname === "/" && href === "/dashboard");
+const SidebarLink = ({ href, icon: Icon, label, isCollapsed }: SidebarLinkProps) => {
+	const pathname = usePathname();
+	const isActive = pathname === href || (pathname === '/' && href === '/dashboard');
 
-  return (
-    <Link href={href}>
-      <div
-        className={`cursor-pointer flex items-center py-4 ${isCollapsed ? 'justify-center' : 'justify-start px-8'} hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${isActive ? 'bg-blue-200' : ''}`}>
-        <Icon className="w-6 h-6 !text-gray-700"/>
-        <span className={`${isCollapsed ? 'hidden' : 'inline-block'} font-medium text-gray-700`}>{label}</span>
-      </div>
-    </Link>
-  )
-}
+	return (
+		<Link href={href}>
+			<div
+				className={`cursor-pointer flex items-center py-4 ${isCollapsed ? 'justify-center' : 'justify-start px-8'} hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${isActive ? 'bg-blue-200' : ''}`}
+			>
+				<Icon className="w-6 h-6 !text-gray-700" />
+				<span className={`${isCollapsed ? 'hidden' : 'inline-block'} font-medium text-gray-700`}>
+					{label}
+				</span>
+			</div>
+		</Link>
+	);
+};
 
 export default SidebarLink;
